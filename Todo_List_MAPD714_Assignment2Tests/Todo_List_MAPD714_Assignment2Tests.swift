94c1e254a56a9a062a1076035f3de932e9cbd6f5
Tejal1386 <tejalmca1386@gmail.com>
2017-12-26 00:20:16 -0500

    Initial Commit

---
+++ Todo_List_MAPD714_Assignment2Tests/Todo_List_MAPD714_Assignment2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Todo_List_MAPD714_Assignment2Tests.swift
+//  Todo_List_MAPD714_Assignment2Tests
+//
+//  Created by Tejal Patel on 2017-12-26.
+//  Copyright © 2017 Centennial College. All rights reserved.
+//
+
+import XCTest
+@testable import Todo_List_MAPD714_Assignment2
+
+class Todo_List_MAPD714_Assignment2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
